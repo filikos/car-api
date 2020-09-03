4f9ce7078bbdda301a4de988cf50a15633fc32dd
Niko Filippidis <nikofilippidis@Nikos-MBP.fritz.box>
2020-09-03 18:15:36 +0200

    WIP: Added cli tool

--- go.mod
+++ go.mod
@@ -7,5 +7,6 @@ require (
 	github.com/joho/godotenv v1.3.0
 	github.com/satori/go.uuid v1.2.0
 	github.com/stretchr/testify v1.6.1
+	github.com/urfave/cli/v2 v2.2.0
 	gopkg.in/check.v1 v0.0.0-20161208181325-20d25e280405 // indirect
 )
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"os"
@@ -10,32 +11,77 @@ import (
 	"workspace-go/coding-challange/car-api/api"
 
 	"workspace-go/coding-challange/car-api/model"
+	"workspace-go/coding-challange/car-api/db"
 
 	"github.com/gorilla/mux"
+	"github.com/urfave/cli/v2"
 )
 
 func main() {
 
-	/*db, err := db.InitDB("./config/dbConfig.env")
-	if err != nil {
-		os.Exit(1)
-	}*/
-
-	mockConnector := api.MockConnector{
-		Data: model.Cars{
-			{ID: "1", Model: "A45", Make: "mercedes", Variant: "amg"},
-			{ID: "2", Model: "C", Make: "mercedes", Variant: "classic"},
-			{ID: "3", Model: "B", Make: "mercedes", Variant: "casual"},
-			{ID: "4", Model: "S", Make: "tesla", Variant: "sport"},
-			{ID: "5", Model: "3", Make: "tesla", Variant: "tour"},
-			{ID: "6", Model: "X", Make: "tesla", Variant: "midnight"},
-			{ID: "7", Model: "Y", Make: "tesla", Variant: "standart"},
+	app := &cli.App{
+		Name:    "Car-Management-API",
+		Version: "v0.0.0",
+		Flags: []cli.Flag{
+			&cli.IntFlag{
+				Name:  "port",
+				Usage: "Port the Rest-API will listen on.",
+				Value: 8080,
+			},
+			&cli.BoolFlag{
+				Name:        "mockmode",
+				Usage:       "Set 'true' to use mocked mode.",
+				Value:       false,
+				DefaultText: "API will use DB connection",
+			},
 		},
 	}
-	
-	service := api.Service{
-		Connector: &mockConnector,
+
+	port := 8080
+	var connector api.Controller
+	app.Action = func(c *cli.Context) error {
+
+		port = c.Int("port")
+		if c.Bool("mockmode") {
+			connector = &api.MockConnector{
+				Data: model.Cars{
+					{ID: "1", Model: "A45", Make: "mercedes", Variant: "amg"},
+					{ID: "2", Model: "C", Make: "mercedes", Variant: "classic"},
+					{ID: "3", Model: "B", Make: "mercedes", Variant: "casual"},
+					{ID: "4", Model: "S", Make: "tesla", Variant: "sport"},
+					{ID: "5", Model: "3", Make: "tesla", Variant: "tour"},
+					{ID: "6", Model: "X", Make: "tesla", Variant: "midnight"},
+					{ID: "7", Model: "Y", Make: "tesla", Variant: "standart"},
+				},
+			}
+		} else {
+			db, err := db.InitDB("./config/dbConfig.env")
+			if err != nil {
+				log.Printf("Failed to connect database. Server will be shut down. Error: %v", err)
+				os.Exit(0)
+			}
+
+			connector = &api.ConnectorDB{
+				Database: *db,
+			}
+		}
+
+		service := api.Service{
+			Connector: connector,
+		}
+
+		startServer(service, port)
+		return nil
 	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
+
+func startServer(service api.Service, port int) {
 
 	r := mux.NewRouter()
 	// TODO: Host to be set here
@@ -48,7 +94,7 @@ func main() {
 
 	server := &http.Server{
 		Handler:      r,
-		Addr:         ":8080",
+		Addr:         fmt.Sprintf(":%v", port),
 		WriteTimeout: time.Second * 15,
 		ReadTimeout:  time.Second * 15,
 		IdleTimeout:  time.Second * 60,
@@ -63,9 +109,9 @@ func main() {
 		}
 	}()
 
-	log.Print("Server Running")
+	log.Printf("Server Running on port: %v", port)
 	<-done
-	log.Print("Server Stopped Running")
-	log.Println("shutting down")
+	log.Println("Server Stopped")
+	log.Println("shutting down...")
 	os.Exit(0)
 }
